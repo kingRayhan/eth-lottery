5610e2dac472c8fb180e333bddbd2e545a3fb325
kingrayhan <rayhan095@gmail.com>
2022-02-03 17:55:11 +0600

    Lottert contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,33 @@
+contract Lottery {
+    address public owner;
+    address payable[] public players;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
+    function join() public payable {
+        require(msg.value >= 1 ether);
+        players.push(payable(msg.sender));
+    }
+
+    function randomNumber() public view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.difficulty, block.timestamp, players)
+                )
+            ) % players.length;
+    }
+
+    function getWinner() public {
+        address self = address(this);
+        uint256 winnerIndex = randomNumber();
+        address payable winner = players[winnerIndex];
+        winner.transfer(self.balance);
+    }
+}
